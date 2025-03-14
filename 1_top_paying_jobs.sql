72ce08ac5fca0b271591f49312353202c97714f8
Geoffrey Barnes <geoffrey.barnes126@gmail.com>
2025-03-14 00:29:50 -0600

    Add files via upload

---
+++ 1_top_paying_jobs.sql
@@ -0,0 +1,18 @@
+/*
+First Question: What are the top-paying data analyst jobs?
+-Identify the top 10 highest-paying Data Analyst roles that are available remotely.
+-Focuses on job postings with specified salaries (remove null values).
+- Why? Highlight the top-paying opportunities for Data Analysts, offering insights into employment options and location flexibility.
+*/
+
+SELECT
+    job_id,
+    job_title,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date
+FROM
+    job_postings_fact
+
+
